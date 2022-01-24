b5338e00716f68b8df5d6538ba7565dd79f96855
THAMIZHMANI-A <you@example.com>
2022-01-24 14:53:53 +0530

    My First Commmit

---
+++ Untitled.m
@@ -0,0 +1,13 @@
+recObj = audiorecorder;
+disp('Start speaking.')
+recordblocking(recObj, 5);
+disp('End of Recording.');
+
+% Play back the recording.
+play(recObj);
+
+% Store data in double-precision array.
+myRecording = getaudiodata(recObj);
+
+% Plot the waveform.
+plot(myRecording);
